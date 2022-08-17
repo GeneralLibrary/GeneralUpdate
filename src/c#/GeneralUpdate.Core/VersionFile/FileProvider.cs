b4e800bafa6a375b3d197a92075de19cad9703d9
Juster.zhu <zhuzhen723723@163.com>
2022-08-18 00:50:08 +0800

    添加b树

--- src/c#/GeneralUpdate.Core/VersionFile/FileProvider.cs
+++ src/c#/GeneralUpdate.Core/VersionFile/FileProvider.cs
@@ -2,10 +2,9 @@
 using System.Collections;
 using System.Collections.Generic;
 
-namespace GeneralUpdate.Core.VersionFile
+namespace GeneralUpdate.Core.Versionfile
 {
     public class FileProvider 
     { 
-
     }
 }
