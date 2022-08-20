573bd4b45c6aabce99bc63e839146b082e4b0a6f
Juster.zhu <zhuzhen723723@163.com>
2022-08-21 00:47:43 +0800

    添加单元测试，添加示例代码

--- src/c#/AutoUpdate.MD5/MainWindow.xaml.cs
+++ src/c#/AutoUpdate.MD5/MainWindow.xaml.cs
@@ -17,48 +17,6 @@ namespace AutoUpdate.MD5
             InitializeComponent();
         }
 
-        #region MD5
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                OpenFileDialog openFile = new OpenFileDialog();
-                bool? isOpen = openFile.ShowDialog(this);
-                if (isOpen != null && isOpen.Value)
-                {
-                    var name = openFile.FileName;
-                    var md5 = GetFileMD5(name);
-                    TxtMD5.Text = md5;
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Failed to get MD5: { ex.Message } ");
-            }
-        }
-
-        private string GetFileMD5(string fileName)
-        {
-            try
-            {
-                var file = new FileStream(fileName, FileMode.Open);
-                var md5 = new MD5CryptoServiceProvider();
-                var retVal = md5.ComputeHash(file);
-                file.Close();
-                var sb = new StringBuilder();
-                for (var i = 0; i < retVal.Length; i++)
-                {
-                    sb.Append(retVal[i].ToString("x2"));
-                }
-                return sb.ToString();
-            }
-            catch (Exception)
-            {
-                return string.Empty;
-            }
-        }
-
-        #endregion MD5
     }
 }
