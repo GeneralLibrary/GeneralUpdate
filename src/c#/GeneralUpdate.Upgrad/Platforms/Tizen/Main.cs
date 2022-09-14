e213e1d8e8397e0140e0da0872fc09d7f3a67915
Juster.zhu <zhuzhen723723@163.com>
2022-09-14 23:05:43 +0800

    添加MAUI示例

---
+++ src/c#/GeneralUpdate.Upgrad/Platforms/Tizen/Main.cs
@@ -0,0 +1,17 @@
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using System;
+
+namespace GeneralUpdate.Upgrad
+{
+    internal class Program : MauiApplication
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            app.Run(args);
+        }
+    }
+}
