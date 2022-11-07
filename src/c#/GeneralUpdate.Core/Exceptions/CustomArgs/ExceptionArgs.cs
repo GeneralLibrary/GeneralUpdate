6adcfb3e60e6a65d5ab10a8549e333f41f76e280
Juster.zhu <zhuzhen723723@163.com>
2022-11-08 01:51:18 +0800

    自定义异常

---
+++ src/c#/GeneralUpdate.Core/Exceptions/CustomArgs/ExceptionArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeneralUpdate.Core.Exceptions.CustomArgs
+{
+    [Serializable]
+    public abstract class ExceptionArgs
+    {
+        public virtual string Message { get { return String.Empty; } }
+    }
+}
---
+++ src/c#/GeneralUpdate.Core/Exceptions/CustomException/GeneralUpdateException.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
+using System.Text;
+
+namespace GeneralUpdate.Core.Exceptions.CustomException
+{
+    /// <summary>
+    /// Exception of GeneralUpdate framework.
+    /// </summary>
+    [Serializable]
+    public sealed class GeneralUpdateException<TExceptionArgs> : Exception, ISerializable
+        where TExceptionArgs : Exception
+    {
+        private const String c_args = "Args";
+        private readonly TExceptionArgs m_args;
+
+        public TExceptionArgs Args { get { return m_args; }  }
+
+        public GeneralUpdateException(String message = null, Exception innerException = null) : this(null, message, innerException) { }
+
+        public GeneralUpdateException(TExceptionArgs args, String message = null, Exception innerException = null) : base(message, innerException) => m_args = args;
+
+        [SecurityPermission(SecurityAction.LinkDemand,Flags =SecurityPermissionFlag.SerializationFormatter)]
+        private GeneralUpdateException(SerializationInfo info, StreamingContext context) : base(info, context) => m_args = (TExceptionArgs)info.GetValue(c_args,typeof(TExceptionArgs));
+
+        [SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.SerializationFormatter)]
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue(c_args, typeof(TExceptionArgs));
+            base.GetObjectData(info, context);
+        }
+
+        public override string Message 
+        {
+            get {
+                String baseMsg = base.Message;
+                return (m_args == null)? baseMsg : $"{baseMsg}({m_args.Message})";
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            GeneralUpdateException<TExceptionArgs> other = obj as GeneralUpdateException<TExceptionArgs>; 
+            if (other == null) return false;
+            return Object.Equals(m_args,other.m_args) && base.Equals(obj);
+        }
+
+        public override int GetHashCode()=> base.GetHashCode();
+    }
+}
