672dd8287e73eaf1114691ef2ff77501035fb416
Juster.zhu <zhuzhen723723@163.com>
2022-11-15 23:45:56 +0800

    提交http异常参数类型

--- src/c#/GeneralUpdate.Core/Exceptions/CustomArgs/HttpExceptionArgs.cs
+++ src/c#/GeneralUpdate.Core/Exceptions/CustomArgs/HttpExceptionArgs.cs
@@ -4,7 +4,31 @@ using System.Text;
 
 namespace GeneralUpdate.Core.Exceptions.CustomArgs
 {
-    internal class HttpExceptionArgs
+    [Serializable]
+    public sealed class HttpExceptionArgs : ExceptionArgs
     {
+        private readonly String _url, _errorMessage;
+        private readonly int _code;
+
+        public HttpExceptionArgs(String url, int code, string errorMessage) 
+        {
+            _url = url;
+            _code = code; 
+            _errorMessage = errorMessage; 
+        }
+
+        public String Url { get { return _url; } }
+
+        public String ErrorMessage { get { return _errorMessage; } }
+
+        public int Code { get { return _code; } }
+
+        public override string Message
+        {
+            get
+            {
+                return (_url == null) ? base.Message : $"Failed to request this address {_url} , status code { _code } , mssage : {_errorMessage}";
+            }
+        }
     }
 }
