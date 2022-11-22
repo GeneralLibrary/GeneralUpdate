779f85818dd4b8f7707359e4b3d9675fc8c52112
Juster.zhu <zhuzhen723723@163.com>
2022-11-22 23:47:59 +0800

    继续完善OSS功能

--- src/c#/GeneralUpdate.Core/OSS/CustomOSS.cs
+++ src/c#/GeneralUpdate.Core/OSS/CustomOSS.cs
@@ -1,22 +1,57 @@
 ﻿using GeneralUpdate.Core.Domain.Entity;
 using GeneralUpdate.Core.Download;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace GeneralUpdate.Core.OSS
 {
     public class CustomOSS : IOSS
     {
+        private string tmepPath = "";
+        private VersionInfo versionInfo;
+        private string _url;
+
         public Task<string> Download()
+        {
+            var manager = new DownloadManager<VersionInfo>(tmepPath, ".zip", 60);
+            manager.MutiAllDownloadCompleted += OnMutiAllDownloadCompleted; ;
+            manager.MutiDownloadCompleted += OnMutiDownloadCompleted; ;
+            manager.MutiDownloadError += OnMutiDownloadError; ;
+            manager.MutiDownloadProgressChanged += OnMutiDownloadProgressChanged; ;
+            manager.MutiDownloadStatistics += OnMutiDownloadStatistics;
+            manager.Add(new DownloadTask<VersionInfo>(manager,versionInfo));
+            manager.LaunchTaskAsync();
+            return Task.FromResult("");
+        }
+
+        private void OnMutiDownloadStatistics(object sender, Bootstrap.MutiDownloadStatisticsEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnMutiDownloadProgressChanged(object csender, Bootstrap.MutiDownloadProgressChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnMutiDownloadError(object sender, Bootstrap.MutiDownloadErrorEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnMutiDownloadCompleted(object sender, Bootstrap.MutiDownloadCompletedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnMutiAllDownloadCompleted(object sender, Bootstrap.MutiAllDownloadCompletedEventArgs e)
         {
             throw new NotImplementedException();
         }
 
         public void SetParmeter(string url)
         {
-            throw new NotImplementedException();
+            _url = url;
         }
     }
 }
--- src/c#/GeneralUpdate.Core/OSS/GeneralUpdateOSS.cs
+++ src/c#/GeneralUpdate.Core/OSS/GeneralUpdateOSS.cs
@@ -1,13 +1,10 @@
-﻿using GeneralUpdate.Core.Domain.DO;
-using GeneralUpdate.Core.Domain.Enum;
-using GeneralUpdate.Core.Exceptions;
+﻿using GeneralUpdate.Core.Domain.Enum;
+using GeneralUpdate.Core.Exceptions.CustomArgs;
+using GeneralUpdate.Core.Exceptions.CustomException;
 using GeneralUpdate.Core.OSS.Strategys;
 using GeneralUpdate.Core.OSS.Strategys.PlatformAndorid;
 using GeneralUpdate.Core.OSS.Strategys.PlatformWindows;
-using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Threading.Tasks;
 
 namespace GeneralUpdate.Core.OSS
@@ -16,22 +13,30 @@ namespace GeneralUpdate.Core.OSS
     {
         public static async Task Start<T>(string url,string appName , string platform = PlatformType.Windows) where T : IOSS, new()
         {
-            IStrategy strategy = null;
-            var oss = new T();
-            oss.SetParmeter(url);
-            string filePath = await oss.Download();
-            switch (platform)
-            {
-                case PlatformType.Windows:
-                    strategy = new WindowsStrategy();
-                    break;
-                case PlatformType.Android:
-                    strategy = new AndoridStrategy();
-                    break;
+            if (string.IsNullOrWhiteSpace(url) || string.IsNullOrWhiteSpace(appName)) throw new ArgumentNullException("The parameter cannot be empty !");
+			try
+			{
+                IStrategy strategy = null;
+                var oss = new T();
+                oss.SetParmeter(url);
+                string versionObjFilePath = await oss.Download();
+                switch (platform)
+                {
+                    case PlatformType.Windows:
+                        strategy = new WindowsStrategy();
+                        break;
+                    case PlatformType.Android:
+                        strategy = new AndoridStrategy();
+                        break;
+                }
+                strategy.Create(versionObjFilePath, appName);
+                strategy.Excute();
+                strategy.StartApp();
             }
-            strategy.Create(filePath, appName);
-            strategy.Excute();
-            strategy.StartApp();
+			catch (Exception ex)
+			{
+				throw new GeneralUpdateException<ExceptionArgs>($"Update failed , { ex.Message } !", ex.InnerException);
+			}
         }
     }
 }
