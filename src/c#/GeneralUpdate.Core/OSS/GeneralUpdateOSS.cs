376fe348e5cffa3b8127fb3e1cfc6ce8c090502d
Juster.zhu <zhuzhen723723@163.com>
2022-11-26 23:45:35 +0800

    添加注释

--- src/c#/GeneralUpdate.Core/OSS/GeneralUpdateOSS.cs
+++ src/c#/GeneralUpdate.Core/OSS/GeneralUpdateOSS.cs
@@ -11,6 +11,16 @@ namespace GeneralUpdate.Core.OSS
 {
     public sealed class GeneralUpdateOSS
     {
+        /// <summary>
+        /// Start OSS update.
+        /// </summary>
+        /// <typeparam name="T">OSS SDK or Custom OSS</typeparam>
+        /// <param name="url">url address</param>
+        /// <param name="appName">main app name</param>
+        /// <param name="platform">platform enum</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="GeneralUpdateException{ExceptionArgs}"></exception>
         public static async Task Start<T>(string url,string appName , string platform = PlatformType.Windows) where T : IOSS, new()
         {
             if (string.IsNullOrWhiteSpace(url) || string.IsNullOrWhiteSpace(appName)) throw new ArgumentNullException("The parameter cannot be empty !");
