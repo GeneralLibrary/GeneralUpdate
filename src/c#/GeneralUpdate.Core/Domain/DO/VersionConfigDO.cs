108c54e3e8885ec44eaeb0693c913bec3e05d176
Juster.zhu <zhuzhen723723@163.com>
2022-12-08 23:44:25 +0800

    添加OSS更新配置文件

--- src/c#/GeneralUpdate.Core/Domain/DO/VersionConfigDO.cs
+++ src/c#/GeneralUpdate.Core/Domain/DO/VersionConfigDO.cs
@@ -1,17 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GeneralUpdate.Core.Domain.DO
+﻿namespace GeneralUpdate.Core.Domain.DO
 {
     public class VersionConfigDO
     {
+        /// <summary>
+        /// Product branch ID (Used to distinguish multiple branches under the same product).
+        /// </summary>
+        public string Guid { get; set; }
+
+        /// <summary>
+        /// Update package download location.
+        /// </summary>
         public string Url { get; set; }
 
+        /// <summary>
+        /// MD5 verification code
+        /// </summary>
         public string MD5 { get; set; }
 
-        public string PacketName { get; set; }
+        /// <summary>
+        /// Update the package name.
+        /// </summary>
+        public string Name { get; set; }
 
+        /// <summary>
+        /// Update the package file format.
+        /// </summary>
+        public string Format { get; set; }
+
+        /// <summary>
+        /// The version number that will be updated.
+        /// </summary>
         public string Version { get; set; }
     }
 }
