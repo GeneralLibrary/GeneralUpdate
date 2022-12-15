6e82cc3e4d5b73d32ac0c505886d0869aa0e3383
Juster.zhu <zhuzhen723723@163.com>
2022-12-15 22:40:11 +0800

    新增控制台打包工具

---
+++ src/c#/GeneralUpdate.PacketCommand/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GeneralUpdate.PacketCommand
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
