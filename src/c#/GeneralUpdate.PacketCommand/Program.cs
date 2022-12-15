8adb585244ff9bcec08f6a5bfac2c79f6270f3e7
Juster.zhu <zhuzhen723723@163.com>
2022-12-16 02:33:58 +0800

    命令行集成

--- src/c#/GeneralUpdate.PacketCommand/Program.cs
+++ src/c#/GeneralUpdate.PacketCommand/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace GeneralUpdate.PacketCommand
+﻿using Colorful;
+using CommandLine.Text;
+using CommandLine;
+using System.Drawing;
+using Console = Colorful.Console;
+
+namespace GeneralUpdate.PacketCommand
 {
     internal class Program
     {
+        private const string fontColor = "#FFF0F5";
+
+        //font : http://www.figlet.org/examples.html
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            System.Console.OutputEncoding = System.Text.Encoding.UTF8;
+            var font = FigletFont.Load("./Resources/slant.flf");
+            var figlet = new Figlet(font);
+            Console.WriteLine(figlet.ToAscii("GeneralUpdate"), ColorTranslator.FromHtml(fontColor));
+            Console.WriteLine("Welcome to use GeneralUpdate PacketCommand tools.", ColorTranslator.FromHtml(fontColor));
+            Console.WriteLine("©2022-2023 JusterZhu. All rights reserved.", ColorTranslator.FromHtml(fontColor));
         }
     }
 }
