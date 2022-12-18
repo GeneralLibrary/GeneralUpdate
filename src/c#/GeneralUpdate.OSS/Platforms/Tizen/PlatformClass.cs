9dfb751c11b738b26ddc888bf047c8ad21715316
Juster.zhu <zhuzhen723723@163.com>
2022-12-18 23:59:01 +0800

    add: miss files

---
+++ src/c#/GeneralUpdate.OSS/Platforms/Tizen/PlatformClass.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace GeneralUpdate.OSS
+{
+    // All the code in this file is only included on Tizen.
+    public class PlatformClass
+    {
+    }
+}
---
+++ src/c#/GeneralUpdate.OSS/Platforms/Windows/PlatformClass.cs
@@ -0,0 +1,7 @@
+﻿namespace GeneralUpdate.OSS
+{
+    // All the code in this file is only included on Windows.
+    public class PlatformClass
+    {
+    }
+}
