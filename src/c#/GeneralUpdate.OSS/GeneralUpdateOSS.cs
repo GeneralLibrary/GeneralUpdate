b46d116c97082081bc66aac1bdaff005e0bc382b
Juster.zhu <zhuzhen723723@163.com>
2022-12-21 21:51:04 +0800

    remove: remove code

--- src/c#/GeneralUpdate.OSS/GeneralUpdateOSS.cs
+++ src/c#/GeneralUpdate.OSS/GeneralUpdateOSS.cs
@@ -12,32 +12,9 @@
         /// <returns></returns>
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="GeneralUpdateException{ExceptionArgs}"></exception>
-   //     public static void Start<T>(string url,string appName ,string fileName = "versions.json" , string format = Format.ZIP, int timeOut = 60, string platform = PlatformType.Windows) where T : IOSS, new()
-   //     {
-   //         if (string.IsNullOrWhiteSpace(url) || string.IsNullOrWhiteSpace(appName)) throw new ArgumentNullException("The parameter cannot be empty !");
-			//try
-			//{
-   //             IStrategy strategy = null;
-   //             var oss = new T();
-   //             oss.SetParameter(url, fileName, format, timeOut);
-   //             oss.Update();
-   //             switch (platform)
-   //             {
-   //                 case PlatformType.Windows:
-   //                     strategy = new WindowsStrategy();
-   //                     break;
-   //                 case PlatformType.Android:
-   //                     strategy = new AndoridStrategy();
-   //                     break;
-   //             }
-   //             strategy.Create(appName);
-   //             strategy.Excute();
-   //             strategy.StartApp();
-   //         }
-			//catch (Exception ex)
-			//{
-			//	throw new GeneralUpdateException<ExceptionArgs>($"Update failed , { ex.Message } !", ex.InnerException);
-			//}
-   //     }
+        public static void Start<T>(string url, string appName, string fileName = "versions.json")
+        {
+            if (string.IsNullOrWhiteSpace(url) || string.IsNullOrWhiteSpace(appName)) throw new ArgumentNullException("The parameter cannot be empty !");
+        }
     }
 }
