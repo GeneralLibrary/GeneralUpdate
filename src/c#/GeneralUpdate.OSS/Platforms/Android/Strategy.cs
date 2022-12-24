5faf85313e9fec60f24b5928dd324944ddcce678
Juster.zhu <zhuzhen723723@163.com>
2022-12-24 14:08:28 +0800

    add： miss file

---
+++ src/c#/GeneralUpdate.OSS/Platforms/Android/Strategy.cs
@@ -0,0 +1,35 @@
+﻿using Android.Content;
+using Android.OS;
+using GeneralUpdate.OSS.Strategys;
+
+namespace GeneralUpdate.OSS
+{
+    // All the code in this file is only included on Android.
+    public class Strategy : IStrategy
+    {
+        private readonly string appPath = FileSystem.AppDataDirectory;
+        private string apk = ".apk";
+
+        public void Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excute()
+        {
+            var file = $"{appPath}/{apk}";
+            var apkFile = new Java.IO.File(file);
+            var intent = new Intent(Intent.ActionView);
+            //Give temporary read permissions.
+            intent.SetFlags(ActivityFlags.GrantReadUriPermission);
+            var uri = FileProvider.GetUriForFile(Android.App.Application.Context, "com.masa.mauidemo.fileprovider", apkFile);
+            //Sets the explicit MIME data type.
+            intent.SetDataAndType(uri, "application/vnd.android.package-archive");
+            //intent.SetDataAndType(Android.Net.Uri.FromFile(new Java.IO.File(file)), "application/vnd.android.package-archive");
+            intent.AddFlags(ActivityFlags.NewTask);
+            Android.App.Application.Context.StartActivity(intent);
+        }
+
+        public string GetVersion() => VersionTracking.CurrentVersion;
+    }
+}
---
+++ src/c#/GeneralUpdate.OSS/Strategys/AbstractStrategy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralUpdate.OSS.Strategys
+{
+    public abstract class AbstractStrategy : IStrategy
+    {
+        private readonly HttpClient _client;
+
+        public async Task DownloadFileAsync(string url,string apk, Action<long, long> action)
+        {
+            var req = new HttpRequestMessage(new HttpMethod("GET"), url);
+            var response = _client.SendAsync(req, HttpCompletionOption.ResponseHeadersRead).Result;
+            var allLength = response.Content.Headers.ContentLength;
+            var stream = await response.Content.ReadAsStreamAsync();
+            var file = $"{FileSystem.AppDataDirectory}/{apk}";
+            await using var fileStream = new FileStream(file, FileMode.Create);
+            await using (stream)
+            {
+                var buffer = new byte[10240];
+                var readLength = 0;
+                int length;
+                while ((length = await stream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                {
+                    readLength += length;
+                    action(readLength, allLength!.Value);
+                    fileStream.Write(buffer, 0, length);
+                }
+            }
+        }
+
+        public abstract void Create();
+
+        public abstract void Excute();
+    }
+}
