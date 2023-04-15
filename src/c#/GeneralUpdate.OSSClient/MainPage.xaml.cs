41667871d66cfd585cf81a171508fbc1b2b1252c
Juster.zhu <zhuzhen723723@163.com>
2023-04-15 23:58:13 +0800

    update .net maui oss test

--- src/c#/GeneralUpdate.OSSClient/MainPage.xaml.cs
+++ src/c#/GeneralUpdate.OSSClient/MainPage.xaml.cs
@@ -16,7 +16,7 @@ namespace GeneralUpdate.OSSClient
             {
                 var url = "http://192.168.50.203";
                 var apk = "GeneralUpdate.Client.apk";
-                var authority = "B8A7FADD-386C-46B0-B283-C9F963420C7C";
+                var authority = "com.generalupdate.oss.fileprovider";
                 var currentVersion = "1.0.0.0";
                 var versionFileName = "version.json";
                 await GeneralUpdateOSS.Start<Strategy>(new ParamsAndroid(url, apk, authority, currentVersion, versionFileName));
