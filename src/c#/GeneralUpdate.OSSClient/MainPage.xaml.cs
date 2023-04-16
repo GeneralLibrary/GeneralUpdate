dc1f385cab3ed878eb1b929eeffea837b340735b
Juster.zhu <zhuzhen723723@163.com>
2023-04-16 21:13:33 +0800

    update manifest.xml

--- src/c#/GeneralUpdate.OSSClient/MainPage.xaml.cs
+++ src/c#/GeneralUpdate.OSSClient/MainPage.xaml.cs
@@ -16,7 +16,7 @@ namespace GeneralUpdate.OSSClient
             {
                 var url = "http://192.168.50.203";
                 var apk = "GeneralUpdate.Client.apk";
-                var authority = "com.generalupdate.oss.fileprovider";
+                var authority = "com.generalupdate.oss";
                 var currentVersion = "1.0.0.0";
                 var versionFileName = "version.json";
                 await GeneralUpdateOSS.Start<Strategy>(new ParamsAndroid(url, apk, authority, currentVersion, versionFileName));
