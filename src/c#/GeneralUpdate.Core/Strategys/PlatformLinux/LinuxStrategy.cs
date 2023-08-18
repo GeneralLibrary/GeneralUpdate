1a7ff988132613e8dc7b8ddfec3d8ce5409d5121
Juster.zhu <zhuzhen723723@163.com>
2023-08-18 22:32:08 +0800

    update strategy

--- src/c#/GeneralUpdate.Core/Strategys/PlatformLinux/LinuxStrategy.cs
+++ src/c#/GeneralUpdate.Core/Strategys/PlatformLinux/LinuxStrategy.cs
@@ -5,5 +5,20 @@ namespace GeneralUpdate.Core.Strategys.PlatformLinux
     public class LinuxStrategy : AbstractStrategy
     {
         public override string GetPlatform() => PlatformType.Linux;
+
+        public override void Create<T>(T parameter)
+        {
+            base.Create(parameter);
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+        }
+
+        public override bool StartApp(string appName, int appType)
+        {
+            return base.StartApp(appName, appType);
+        }
     }
 }
--- src/c#/GeneralUpdate.Core/Strategys/PlatformMac/MacStrategy.cs
+++ src/c#/GeneralUpdate.Core/Strategys/PlatformMac/MacStrategy.cs
@@ -5,5 +5,20 @@ namespace GeneralUpdate.Core.Strategys.PlatformMac
     public class MacStrategy : AbstractStrategy
     {
         public override string GetPlatform() => PlatformType.Mac;
+
+        public override void Create<T>(T parameter)
+        {
+            base.Create(parameter);
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+        }
+
+        public override bool StartApp(string appName, int appType)
+        {
+            return base.StartApp(appName, appType);
+        }
     }
 }
