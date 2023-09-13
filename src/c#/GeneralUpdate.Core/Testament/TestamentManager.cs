d9797bba78129946f262d39af8626efc7a3192ac
Juster.zhu <zhuzhen723723@163.com>
2023-09-13 22:29:42 +0800

    Update the last words mechanism

--- src/c#/GeneralUpdate.Core/Testament/TestamentManager.cs
+++ src/c#/GeneralUpdate.Core/Testament/TestamentManager.cs
@@ -20,23 +20,37 @@ namespace GeneralUpdate.Core.Testament
             _testamentPath = Path.Combine(_path, TESTAMENT);
         }
 
+        /// <summary>
+        /// Before the update, the files that need to be updated are backed up by a complete directory structure.
+        /// </summary>
+        /// <param name="files"></param>
         public void Preconditioning(List<string> files)
         {
             
         }
 
+        /// <summary>
+        /// When the program is started, the contents of the last note will be analyzed to restore the backup or continue to update the last note.
+        /// </summary>
         public void Demolish()
         {
             _testamentPO = FileUtil.ReadJsonFile<TestamentPO>(_testamentPath);
             File.Delete(_testamentPath);
         }
 
+        /// <summary>
+        /// Generate the contents of the last word, read the last word when the next program starts for backup restoration or re-update.
+        /// </summary>
+        /// <param name="testament"></param>
         public void Build(TestamentPO testament)
         {
             FileUtil.CreateJsonFile(_testamentPath, TESTAMENT, _testamentPO);
             Dump();
         }
 
+        /// <summary>
+        /// Export the dump file of the current application when an unknown exception occurs.
+        /// </summary>
         public void Dump() {
             var currentProcess = Process.GetCurrentProcess();
             int pid = currentProcess.Id;
