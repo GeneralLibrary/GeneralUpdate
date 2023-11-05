c5d31fd90f83e868b38eff0141ba63eda74ee8c0
Juster.zhu <zhuzhen723723@163.com>
2023-11-05 22:59:48 +0800

    Create BackupBuilder.cs

---
+++ src/c#/GeneralUpdate.Differential/Backups/BackupBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeneralUpdate.Differential.Backups
+{
+    public class BackupBuilder
+    {
+        public void Backup() { }
+
+        public void Restore() { }
+
+    }
+}
