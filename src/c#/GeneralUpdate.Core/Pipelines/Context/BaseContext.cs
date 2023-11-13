8c85364bf84bc78748a887a8b53dac5d2dcffffb
Juster.zhu <zhuzhen723723@163.com>
2023-11-13 21:44:37 +0800

    Remove redundant code

--- src/c#/GeneralUpdate.Core/Pipelines/Context/BaseContext.cs
+++ src/c#/GeneralUpdate.Core/Pipelines/Context/BaseContext.cs
@@ -8,48 +8,6 @@ namespace GeneralUpdate.Core.Pipelines.Context
     /// <summary>
     /// Pipeline common content.
     /// </summary>
-    //public class BaseContext
-    //{
-    //    public VersionInfo Version { get; set; }
-
-    //    public string Name { get; set; }
-
-    //    public string ZipfilePath { get; set; }
-
-    //    public string TargetPath { get; set; }
-
-    //    public string SourcePath { get; set; }
-
-    //    public string Format { get; set; }
-
-    //    public Encoding Encoding { get; set; }
-
-    //    /// <summary>
-    //    /// Blacklist file list.
-    //    /// </summary>
-    //    public List<string> BlackFiles { get; set; }
-
-    //    /// <summary>
-    //    /// Blacklist File suffix list.
-    //    /// </summary>
-    //    public List<string> BlackFileFormats { get; set; }
-
-    //    public BaseContext(VersionInfo version, string zipfilePath, string targetPath, string sourcePath, string format, Encoding encoding, List<string> files, List<string> fileFormats)
-    //    {
-    //        Version = version ?? throw new ArgumentNullException($"{nameof(VersionInfo)} Cannot be empty");
-    //        ZipfilePath = string.IsNullOrWhiteSpace(zipfilePath) ? throw new ArgumentNullException($"{nameof(zipfilePath)} Cannot be empty") : zipfilePath;
-    //        TargetPath = string.IsNullOrWhiteSpace(targetPath) ? throw new ArgumentNullException($"{nameof(targetPath)} Cannot be empty") : targetPath; ;
-    //        SourcePath = string.IsNullOrWhiteSpace(sourcePath) ? throw new ArgumentNullException($"{nameof(sourcePath)} Cannot be empty") : sourcePath; ;
-    //        Format = string.IsNullOrWhiteSpace(format) ? throw new ArgumentNullException($"{nameof(format)} Cannot be empty") : format;
-    //        Encoding = encoding;
-    //        BlackFiles = files;
-    //        BlackFileFormats = fileFormats;
-    //    }
-    //}
-
-    /// <summary>
-    /// Pipeline common content.
-    /// </summary>
     public class BaseContext
     {
         public VersionInfo Version { get; private set; }
--- src/c#/GeneralUpdate.Core/Strategys/PlatformWindows/WindowsStrategy.cs
+++ src/c#/GeneralUpdate.Core/Strategys/PlatformWindows/WindowsStrategy.cs
@@ -10,7 +10,6 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace GeneralUpdate.Core.Strategys.PlatformWindows
