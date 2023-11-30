c0a3012b28c7aab7068e0b147a7f0e5b3e9a25e4
Juster Zhu <zhuzhen723723@163.com>
2023-11-30 21:33:07 +0800

    Add a process information class

---
+++ src/c#/GeneralUpdate.MiniBowl/Model/ProcessInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralUpdate.MiniBowl.Model
+{
+    public enum ProcessStatus 
+    {
+        Run,
+        None,
+        Down
+    }
+
+    internal class ProcessInfo
+    {
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+
+        public ProcessStatus Status { get; set; }
+
+        public string Path { get; set; }
+    }
+}
