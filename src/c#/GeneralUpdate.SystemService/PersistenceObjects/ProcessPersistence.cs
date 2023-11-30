d835af432cc2e0320bbb49f180eeae0e3b21030c
Juster Zhu <zhuzhen723723@163.com>
2023-11-30 23:44:41 +0800

    Add update system service

---
+++ src/c#/GeneralUpdate.SystemService/PersistenceObjects/ProcessPersistence.cs
@@ -0,0 +1,31 @@
+﻿namespace GeneralUpdate.SystemService.PersistenceObjects
+{
+    internal enum ProcessStatus 
+    {
+        /// <summary>
+        /// Processing has not yet begun.
+        /// </summary>
+        NotStarted,
+        /// <summary>
+        /// Processing completed.
+        /// </summary>
+        Completed,
+        /// <summary>
+        /// Processing failure.
+        /// </summary>
+        Failed
+    }
+
+    internal class ProcessPersistence
+    {
+        public required string Name { get; set; }
+
+        public required string Path { get; set; }
+
+        public required string BackupPath { get; set; }
+
+        public ProcessStatus Status { get; set; }
+
+        public DateTime CreateTime { get; set; }
+    }
+}
---
+++ src/c#/GeneralUpdate.SystemService/PersistenceObjects/WillMessagePersistence.cs
@@ -0,0 +1,7 @@
+﻿namespace GeneralUpdate.SystemService.PersistenceObjects
+{
+    internal class WillMessagePersistence<T> where T : class
+    {
+        public required List<T> Messages { get; set; }
+    }
+}
