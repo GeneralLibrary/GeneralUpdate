17488a51f16030f8887e1e4e212de22b9c65bbc4
Juster Zhu <zhuzhen723723@163.com>
2023-12-23 19:56:39 +0800

    feature: Test and modify the driver update function

--- src/c#/GeneralUpdate.Client/Program.cs
+++ src/c#/GeneralUpdate.Client/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Read();
         }
     }
 }
--- src/c#/GeneralUpdate.Core/Driver/CommandExecutor.cs
+++ src/c#/GeneralUpdate.Core/Driver/CommandExecutor.cs
@@ -11,26 +11,24 @@ namespace GeneralUpdate.Core.Driver
         public static void ExecuteCommand(string command)
         {
             /*
+             *
              *Problems may occur, including:
 Permission issues: PnPUtil requires administrator rights to run. If you try to run it without the proper permissions, the backup or restore may fail.
 Driver compatibility: Although the backed up drivers work properly at backup time, if the operating system is upgraded, the backed up drivers may no longer be compatible with the new operating system version.
 Hardware problems: If the hardware device fails or the hardware configuration changes, the backup driver may not work properly.
 
-To minimize these risks, the following measures are recommended:
+               To minimize these risks, the following measures are recommended:
 Before doing anything, create a system restore point so that it can be restored to its previous state if something goes wrong.
 Update the driver regularly to ensure that the driver is compatible with the current operating system version.
 If possible, use pre-tested drivers that are proven to work.
              * 
              */
-            
             var processStartInfo = new ProcessStartInfo
             {
                 WindowStyle = ProcessWindowStyle.Hidden,
                 FileName = "cmd.exe",
                 Arguments = command,
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                CreateNoWindow = true,
+                UseShellExecute = true,
                 Verb = "runas"
             };
 
@@ -40,7 +38,7 @@ If possible, use pre-tested drivers that are proven to work.
                 process.WaitForExit();
 
                 if (process.ExitCode != 0)
-                    throw new Exception("Operation failed: " + process.StandardOutput.ReadToEnd());
+                    throw new Exception($"Operation failed code: {process.ExitCode}");
             }
         }
     }
