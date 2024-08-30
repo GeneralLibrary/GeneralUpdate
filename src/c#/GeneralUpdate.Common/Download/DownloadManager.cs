964e02a1c90c634bb5f20e03c20ab16d1e4800b0
justerzhu <zhuzhen723723@outlook.com>
2024-08-30 23:52:52 +0800

    修改

--- src/c#/GeneralUpdate.Common/Download/DownloadManager.cs
+++ src/c#/GeneralUpdate.Common/Download/DownloadManager.cs
@@ -9,28 +9,23 @@ namespace GeneralUpdate.Common.Download
 {
     public sealed class DownloadManager<TVersion>
     {
+        private List<TVersion> _versions;
         private ImmutableList<Task<TVersion>> _downloadTasks;
         
-        //具有通知能力，通知下载进度、下载速度、下载文件大小、下载文件信息、单个文件下载状态、所有文件下载状态
-        //异常下载中断可以继续尝试下载吗
-        //设置一组Uri可以同时下载
-        //多线程异步流同时下载多个文件的且具有断点续传功能
-        //超时时间
-
-        public DownloadManager()
+        public DownloadManager(List<TVersion> versions)
         {
+            _versions = versions;
             _downloadTasks = ImmutableList.Create<Task<TVersion>>();
         }
         
-        
         /*public async Task DownloadFilesAsync()
         {
-            foreach (var file in _downloadTasks)
+            foreach (var version in _versions)
             {
                 _downloadTasks.Add(DownloadFileWithResumeAsync(file.Url, file.FilePath));
             }
 
-            await Task.WhenAll(downloadTasks);
+            await Task.WhenAll(_downloadTasks);
         }*/
 
         private async Task DownloadFileWithResumeAsync(string url, string filePath)
