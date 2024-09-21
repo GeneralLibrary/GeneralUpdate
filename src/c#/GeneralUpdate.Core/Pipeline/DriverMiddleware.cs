7c479c0b82012f9cbb7012947bc6d6858f6cca51
Juster.zhu <zhuzhen723723@163.com>
2024-09-21 22:56:34 +0800

    Update DriverMiddleware.cs

--- src/c#/GeneralUpdate.Core/Pipeline/DriverMiddleware.cs
+++ src/c#/GeneralUpdate.Core/Pipeline/DriverMiddleware.cs
@@ -1,5 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 using GeneralUpdate.Common.Internal.Pipeline;
+using GeneralUpdate.Core.Driver;
 
 namespace GeneralUpdate.Core.Pipeline;
 
@@ -7,6 +10,24 @@ public class DriverMiddleware : IMiddleware
 {
     public Task InvokeAsync(PipelineContext context)
     {
-        throw new System.NotImplementedException();
+        return Task.Run(() =>
+        {
+            var drivers = context.Get<List<string>>("Drivers");
+            var sourcesPath = context.Get<string>("Sources");
+            var targetPath = context.Get<string>("Target");
+            var version = context.Get<string>("Version");
+        
+            var information = new DriverInformation.Builder()
+                .SetInstallDirectory(Path.Combine(sourcesPath, version))
+                .SetOutPutDirectory(Path.Combine(targetPath, version))
+                .SetDriverNames(drivers)
+                .Build();
+
+            var processor = new DriverProcessor();
+            processor.AddCommand(new BackupDriverCommand(information));
+            processor.AddCommand(new DeleteDriverCommand(information));
+            processor.AddCommand(new InstallDriverCommand(information));
+            processor.ProcessCommands();
+        });
     }
 }
