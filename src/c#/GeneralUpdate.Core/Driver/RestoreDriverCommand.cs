7dda81e20efd338b1dfcac6e4085333360b276dd
Juster.zhu <zhuzhen723723@163.com>
2024-09-21 23:17:08 +0800

    Update RestoreDriverCommand.cs

--- src/c#/GeneralUpdate.Core/Driver/RestoreDriverCommand.cs
+++ src/c#/GeneralUpdate.Core/Driver/RestoreDriverCommand.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace GeneralUpdate.Core.Driver
 {
-    public class RestoreDriverCommand : IDriverCommand
+    public class RestoreDriverCommand
     {
         private DriverInformation _information;
 
