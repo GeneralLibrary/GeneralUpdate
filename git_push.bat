589f39120b9959d3e1be39a92b5f22d28a909e30
justerzhu <zhuzhen723723@outlook.com>
2025-01-08 16:07:56 +0800

    update readme

--- git_push.bat
+++ git_push.bat
@@ -16,7 +16,7 @@ if %errorlevel% neq 0 (
     exit /b %errorlevel%
 )
 
-echo Pushing to upstream remote on 'main' branch...
+echo Pushing to upstream remote on 'master' branch...
 git push upstream master
 if %errorlevel% neq 0 (
     echo Failed to push to upstream remote on 'main' branch.
@@ -24,7 +24,7 @@ if %errorlevel% neq 0 (
     exit /b %errorlevel%
 )
 
-echo Pushing to 'upstream_gitcode' remote on 'main' branch...
+echo Pushing to 'upstream_gitcode' remote on 'master' branch...
 git push upstream_gitcode master
 if %errorlevel% neq 0 (
     echo Failed to push to 'upstream_gitcode' remote on 'main' branch.
