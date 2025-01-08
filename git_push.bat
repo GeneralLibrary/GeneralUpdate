1a5e67e1ce6183149614d2c46864e2ae64e742c3
justerzhu <zhuzhen723723@outlook.com>
2025-01-08 16:05:47 +0800

    update readme.md

---
+++ git_push.bat
@@ -0,0 +1,36 @@
+@echo off
+
+git remote -v
+
+set /p commitmessage=Git commit message:
+
+git add .
+
+git commit -m "%commitMessage%"
+
+echo Pushing to default remote repository...
+git push
+if %errorlevel% neq 0 (
+    echo Failed to push to default remote repository.
+    pause
+    exit /b %errorlevel%
+)
+
+echo Pushing to upstream remote on 'main' branch...
+git push upstream master
+if %errorlevel% neq 0 (
+    echo Failed to push to upstream remote on 'main' branch.
+    pause
+    exit /b %errorlevel%
+)
+
+echo Pushing to 'upstream_gitcode' remote on 'main' branch...
+git push upstream_gitcode master
+if %errorlevel% neq 0 (
+    echo Failed to push to 'upstream_gitcode' remote on 'main' branch.
+    pause
+    exit /b %errorlevel%
+)
+
+echo All pushes completed successfully.
+pause
