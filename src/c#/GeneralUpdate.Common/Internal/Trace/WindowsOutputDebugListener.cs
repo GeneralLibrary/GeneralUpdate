c6e115dc5388d433901d4d3a1ddf4756ae971854
Juster Zhu <zhuzhen723723@163.com>
2025-08-27 15:45:16 +0800

    Add XML doc for OutputDebugString usage
    Added a summary comment explaining the compatibility and usage of OutputDebugString in .NET AOT environments on Windows, including platform and declaration requirements.

--- src/c#/GeneralUpdate.Common/Internal/Trace/WindowsOutputDebugListener.cs
+++ src/c#/GeneralUpdate.Common/Internal/Trace/WindowsOutputDebugListener.cs
@@ -6,6 +6,13 @@ namespace GeneralUpdate.Common.Internal;
 
 public class WindowsOutputDebugListener : TraceListener
 {
+    /// <summary>
+    /// Does not affect .NET AOT compilation and runtime on the Windows platform, provided that the following conditions are met:
+    ///     The target platform is restricted to Windows (due to the dependency on kernel32.dll);
+    ///     The function declaration is static (not dynamically generated).
+    /// This syntax is safe in an AOT environment and can properly work with Dbgview.exe to capture logs.
+    /// </summary>
+    /// <param name="lpOutputString"></param>
     [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
     private static extern void OutputDebugString(string lpOutputString);
 
