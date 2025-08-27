e2004edd887c2445cd925597b1e8f285c07f8444
Juster Zhu <zhuzhen723723@163.com>
2025-08-27 16:20:28 +0800

    Remove MD5 and SHA1 hash algorithm implementations
    Deleted Md5HashAlgorithm and Sha1HashAlgorithm classes and removed their references from project files. This likely improves security by eliminating outdated and insecure hash algorithms from the codebase.

--- src/c#/GeneralUpdate.Common/HashAlgorithms/Md5HashAlgorithm.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Security.Cryptography;
-
-namespace GeneralUpdate.Common.HashAlgorithms
-{
-    public class Md5HashAlgorithm : HashAlgorithmBase
-    {
-        protected override HashAlgorithm GetHashAlgorithm() => MD5.Create();
-    }
-}
--- src/c#/GeneralUpdate.Common/HashAlgorithms/Sha1HashAlgorithm.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Security.Cryptography;
-
-namespace GeneralUpdate.Common.HashAlgorithms
-{
-    public class Sha1HashAlgorithm : HashAlgorithmBase
-    {
-        protected override HashAlgorithm GetHashAlgorithm() => new SHA1Managed();
-    }
-}
