a11d1c8c7cab04979363680838ac938f496924bb
Juster Zhu <zhuzhen723723@163.com>
2026-01-26 18:25:18 +0800

    add Extension examples

---
+++ src/c#/GeneralUpdate.Ext/Program.cs
@@ -0,0 +1,31 @@
+﻿using GeneralUpdate.Extension;
+using GeneralUpdate.Extension.Metadata;
+
+namespace GeneralUpdate.Ext;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var installPath = @"C:\data\install";
+        var downloadPath = @"C:\data\download";
+        var host = new ExtensionHost(new Version("1.0.0"), installPath, downloadPath);
+        host.InstallationCompleted += (_, _) =>
+        {
+            Console.WriteLine("Installation completed");
+        };
+        host.DownloadCompleted += (_, _) =>
+        {
+            Console.WriteLine("Download completed");
+        };
+        host.DownloadProgress += (_, _) =>
+        {
+            Console.WriteLine("Downloading...");
+        };
+
+        host.QueueUpdate(new AvailableExtension());
+        await host.ProcessNextUpdateAsync();
+
+        Console.Read();
+    }
+}
